6a34fbbb6b4d5544b363269eefcb7add641c6398
Manuri-Amasha <manuriamasha99@gmail.com>
2025-10-29 02:58:07 +0530

    add token validations for activity routes

--- routes/activity.routes.js
+++ routes/activity.routes.js
@@ -1,20 +1,30 @@
 import express from "express";
 import activityController from "../controllers/activity.controller.js";
 import upload from "../middlewares/upload.middleware.js";
+import tokenMiddleware from "../middlewares/token.middleware.js";
 
 const router = express.Router();
 
 router.post(
   "/create",
+  tokenMiddleware.verifyToken,
+  tokenMiddleware.authorizeAdmin,
   upload.single("logo"),
   activityController.createActivity
 );
 router.put(
   "/update/:id",
+  tokenMiddleware.verifyToken,
+  tokenMiddleware.authorizeAdmin,
   upload.single("logo"),
   activityController.updateActivity
 );
-router.delete("/:id", activityController.deleteActivity);
+router.delete(
+  "/:id",
+  tokenMiddleware.verifyToken,
+  tokenMiddleware.authorizeAdmin,
+  activityController.deleteActivity
+);
 router.get("/get-all", activityController.getAllActivities);
 router.get("/:id", activityController.getActivityById);
 
