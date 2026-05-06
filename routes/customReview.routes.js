cf7340288211ae0ae4b932824078884f10ba216f
chamika <chamika@avenstra.lk>
2026-05-07 02:37:54 +0530

    authorize apis

--- routes/customReview.routes.js
+++ routes/customReview.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import customReviewController from "../controllers/customReview.controller.js";
+import tokenMiddleware from "../middlewares/token.middleware.js";
 
 const router = express.Router();
 
 router.post("/create", customReviewController.createCustomReview);
 router.get("/get-all", customReviewController.getAllCustomReviews);
-router.get("/:id", customReviewController.getCustomReviewById);
-router.put("/update/:id", customReviewController.updateCustomReview);
-router.delete("/:id", customReviewController.deleteCustomReview);
+router.get("/:id",tokenMiddleware.verifyToken,tokenMiddleware.authorizeAdmin, customReviewController.getCustomReviewById);
+router.put("/update/:id",tokenMiddleware.verifyToken,tokenMiddleware.authorizeAdmin, customReviewController.updateCustomReview);
+router.delete("/:id",tokenMiddleware.verifyToken,tokenMiddleware.authorizeAdmin, customReviewController.deleteCustomReview);
 
 export default router;
