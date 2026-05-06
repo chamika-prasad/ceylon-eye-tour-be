b1f24678221cfc3d62489e1652a278781d0e0c26
chamika <chamika@avenstra.lk>
2026-05-07 02:07:27 +0530

    add new repository for custom review

---
+++ repositories/customReview.repository.js
@@ -0,0 +1,33 @@
+import { CustomReview } from "./../models/index.js";
+
+const createCustomReview = async (data) => {
+  return await CustomReview.create(data);
+};
+
+const updateCustomReview = async (id, data) => {
+  return await CustomReview.update(data, {
+    where: { id },
+  });
+};
+
+const deleteCustomReview = async (id) => {
+  return await CustomReview.destroy({
+    where: { id },
+  });
+};
+
+const getAllCustomReviews = async () => {
+  return await CustomReview.findAll();
+};
+
+const getCustomReviewById = async (id) => {
+  return await CustomReview.findByPk(id);
+};
+
+export {
+  createCustomReview,
+  updateCustomReview,
+  deleteCustomReview,
+  getAllCustomReviews,
+  getCustomReviewById,
+};
