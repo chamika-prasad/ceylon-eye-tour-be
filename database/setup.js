77fc2f34ec40504a1d322cc90749fcccb6675b93
chamika <chamika@avenstra.lk>
2026-05-24 21:07:10 +0530

    fix : update setup js

--- database/setup.js
+++ database/setup.js
@@ -297,11 +297,7 @@ const SQL_STATEMENTS = [
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (first_payment_id) REFERENCES payments(id)
-)`,
-`ALTER TABLE payments 
-MODIFY COLUMN status ENUM('success', 'pending', 'canceled', 'failed', 'chargedback', 'refund') DEFAULT 'pending'`,
-`ALTER TABLE second_payments 
-MODIFY COLUMN status ENUM('success', 'pending', 'canceled', 'failed', 'chargedback', 'refund') DEFAULT 'pending'`
+)`
 ];
 
 async function setupDatabase() {
